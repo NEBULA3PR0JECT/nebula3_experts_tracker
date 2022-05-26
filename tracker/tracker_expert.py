68f7f63fa12ea46336fc78d73bd0622d7589ee46
aharonamir <aharon.amir@gmail.com>
2022-05-26 11:40:57 +0000

    docker for microservice and updated env file

--- tracker/tracker_expert.py
+++ tracker/tracker_expert.py
@@ -4,6 +4,26 @@ from nebula3_experts.service.base_expert import BaseExpert
 from nebula3_experts.app import ExpertApp
 from nebula3_experts.common.models import ExpertParam
 
+""" Predict params
+@param: detect_every: how many frames to track before accepting detection model detections.
+@param: merge_iou_threshold: the IOU score threhold for merging items during tracking.
+@param: refresh_on_detect: if True, removes all tracked items that were not found by the detection model.
+@param: tracker_type - TRACKER_TYPE_KCF / TRACKER_TYPE_CSRT
+@param: batch_size
+"""
+
+"""backend & models
+VideoPredictor init params:
+- tracker backend: detectron or tflow
+- pretrained_model_cfg - default: CFG_COCO_DETECTION_FerRCNN_X101_32x8d_FPN_LR3x
+- confidence_threshold - default 0.5
+
+CFG_DEFAULT = {
+    at.BACKEND_TFLOW: 'CFG_OID_V4_DETECTION_FerRCNN_INCEPTION_V2',
+    at.BACKEND_DETECTRON: 'CFG_COCO_DETECTION_FerRCNN_X101_32x8d_FPN_LR3x'
+}
+"""
+
 class TrackerExpert(BaseExpert):
     def __init__(self):
         super().__init__()
